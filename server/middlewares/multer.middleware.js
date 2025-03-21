d0e411432aff57e5ce7de63da76da6218b206318
pyush-18 <piyushjoshi1812@gmail.com>
2025-03-21 14:11:50 +0530

    updating multer

--- server/middlewares/multer.middleware.js
+++ server/middlewares/multer.middleware.js
@@ -1,12 +1,19 @@
 import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const uploadDir = path.join(process.cwd(), "public/temp");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb){
-        cb(null, "../public/temp")
-    },
-    filename: function(req, file, cb){
-        cb(null, file.originalname)
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 
-export const upload = multer({storage: storage})
+export const upload = multer({ storage: storage });
